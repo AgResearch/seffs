89aef8daaf26f57bd110a5fb63c57f3974f5ba83
Simon Guest <simon.guest@agresearch.co.nz>
2025-03-17 11:02:59 +1300

    wrapped-seffs

--- flake.nix
+++ flake.nix
@@ -13,7 +13,37 @@
             inherit system;
           };
 
-          seffs = pkgs.writeShellScriptBin "seffs" (builtins.readFile ./seffs);
+          simple-seffs = pkgs.writeScriptBin "seffs" (builtins.readFile ./seffs);
+
+          wrapped-seffs = pkgs.stdenv.mkDerivation {
+            name = "seffs";
+
+            src = ./seffs;
+
+            buildInputs = with pkgs; [ elvish nushell ];
+            nativeBuildInputs = [ pkgs.makeWrapper ];
+
+            dontUnpack = true;
+            dontBuild = true;
+
+            installPhase = ''
+              mkdir -p $out/bin
+              runHook preInstall
+              cp $src $out/bin/seffs
+              chmod 755 $out/bin/seffs
+              runHook postInstall
+            '';
+
+            postFixup = with pkgs; ''
+              wrapProgram $out/bin/seffs --set PATH ${lib.makeBinPath [
+                coreutils
+                elvish
+                nushell
+              ]}:/usr/bin
+            '';
+          };
+
+          seffs = wrapped-seffs;
 
         in
         with pkgs;
